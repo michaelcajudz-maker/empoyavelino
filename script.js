01a46227375833923c4c5278a8e0f4e2746ad87e
michaelcajudz-maker <michaelcajudz@gmail.com>
2026-07-09 19:20:59 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 // LASTEST GS URL HERE
-var LATEST_PROJ = "https://script.google.com/macros/s/AKfycbwA9nlhudxYQfLbuWEKvre8qhZMhpDMdXGHoXsITODRJuhLgtqII_Y_qXLpDxBM8Sv0/exec"; // <-------
+var LATEST_PROJ = "https://script.google.com/macros/s/AKfycby7Fo4KzIW5HcMEoTBoJkA-k1US-ExX30q2NycipVgo5EaxC-leuN4w2gvQCvnJRJ8z/exec"; // <-------
 
 var s = new URLSearchParams(window.location.search).get('s');
 var u = s ? LATEST_PROJ + "?s=" + s : LATEST_PROJ ; 
